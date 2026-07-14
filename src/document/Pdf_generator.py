5576c88cc97a57e39d79d09eeb680b9917d1a4c0
Sharman Jeurkar <sharmanjeurkar@gmail.com>
2026-07-14 10:20:16 +0530

    added lambda support for playright and configured docker to create image for lambda function

--- src/document/Pdf_generator.py
+++ src/document/Pdf_generator.py
@@ -44,7 +44,7 @@ def generate_invoice_pdf(
 
     print("🌐 Spinning up headless browser...")
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(args=["--no-sandbox", "--disable-dev-shm-usage"])
         page = browser.new_page()
 
         # Playwright renders the injected HTML string
