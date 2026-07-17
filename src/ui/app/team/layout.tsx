f1d3aee5e3bf48fe72b3f7d911ac0b30aa9460a6
Sharman Jeurkar <sharmanjeurkar@gmail.com>
2026-07-17 09:03:28 +0530

    setting up componant for MFA authenticator

---
+++ src/ui/app/team/layout.tsx
@@ -0,0 +1,10 @@
+// app/login/layout.tsx
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+};
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
